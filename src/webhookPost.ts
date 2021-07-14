9dd4f2478b85c78dfe7a5d2ef41c5d5ede7c2f2e
daniloab <daniloassis.ti@gmail.com>
2021-07-14 20:14:36 -0300

    feat(donation): adding status to simulate webhook update

--- src/webhookPost.ts
+++ src/webhookPost.ts
@@ -1,45 +1,46 @@
-import { ParameterizedContext } from 'koa';
-import { debugConsole } from './debugConsole';
-import Donation, { DONATION_STATUS } from './modules/donation/DonationModel';
-import { Types } from 'mongoose';
+import { ParameterizedContext } from "koa";
+import { debugConsole } from "./debugConsole";
+import Donation, { DONATION_STATUS } from "./modules/donation/DonationModel";
+import { Types } from "mongoose";
 
 export type WebhookPostBody = {
-  charge: ChargesItem,
-  pix: PixItem
-}
-export type ChargesItem = {
-  _id: string
-  status: string
-  customer: null
-  value: number
-  comment: string
-  createdBy: string
-  company: string
-  transactionID: string
-  correlationID: string
-  createdAt: string
-  updatedAt: string
-}
+  pixQrCode: PixQrCodeItem;
+  pix: PixItem;
+};
+export type PixQrCodeItem = {
+  _id: string;
+  name: string;
+  identifier: string;
+  value: number;
+  comment: string;
+  createdBy: string;
+  company: string;
+  correlationID: string;
+  createdAt: string;
+  updatedAt: string;
+};
 export type PixItem = {
-  _id: string
-  charge: string
-  time: string
-  value: number
-  transactionID: string
-  infoPagador: string
-  raw: Raw
-}
+  _id: string;
+  pixQrCode: string;
+  time: string;
+  value: number;
+  transactionID: string;
+  infoPagador: string;
+  raw: Raw;
+};
 export type Raw = {
-  horario: string
-  valor: string
-  endToEndId: string
-  txid: string
-  infoPagador: string
-}
+  horario: string;
+  valor: string;
+  endToEndId: string;
+  txid: string;
+  infoPagador: string;
+};
 
-export const webhookSecret = 'Do not tell anyone';
+export const webhookSecret = "Do not tell anyone";
 
-export const webhookPost = async (ctx: ParameterizedContext<{}, {}, WebhookPostBody>) => {
+export const webhookPost = async (
+  ctx: ParameterizedContext<{}, {}, WebhookPostBody>
+) => {
   debugConsole({
     body: ctx.request.body,
     params: ctx.params,
@@ -50,29 +51,25 @@ export const webhookPost = async (ctx: ParameterizedContext<{}, {}, WebhookPostB
   if (ctx.headers.authorization !== webhookSecret) {
     ctx.status = 401;
     ctx.body = {
-      error: 'unauthorized',
+      error: "unauthorized",
     };
     return;
   }
 
   // eslint-disable-next-link
-  const { charge, pixTransaction } = ctx.request.body;
+  const { pixQrCode, pix } = ctx.request.body;
+
+  console.log("pix", pix);
+  console.log("pixQrCode", pixQrCode);
 
   // webhook payload of test
-  if (!charge && !pixTransaction) {
+  if (!pixQrCode && !pixTransaction) {
     ctx.status = 200;
     return;
   }
 
-  if (!Types.ObjectId.isValid(charge.correlationID)) {
-    console.log('invalid correlationID: ', {
-      charge,
-    });
-    return;
-  }
-
   const donation = await Donation.findOne({
-    _id: charge.correlationID,
+    identifier: pixQrCode.identifier,
   });
 
   if (!donation) {
@@ -81,17 +78,36 @@ export const webhookPost = async (ctx: ParameterizedContext<{}, {}, WebhookPostB
     return;
   }
 
-  // donation was paied
-  if (charge.status === 'COMPLETED') {
-    await Donation.updateOne({
-      _id: donation._id,
-    }, {
-      $set: {
-        status: DONATION_STATUS.PAIED,
+  if (
+    donation.identifier === pixQrCode.identifier &&
+    donation.identifier === pix.transactionID
+  ) {
+    await Donation.updateOne(
+      {
+        _id: donation._id,
       },
-    });
+      {
+        $set: {
+          status: DONATION_STATUS.PAIED,
+        },
+      }
+    );
   }
+
+  // @todo improve this
+  // if (pixQrCode.status === "COMPLETED") {
+  //   await Donation.updateOne(
+  //     {
+  //       _id: donation._id,
+  //     },
+  //     {
+  //       $set: {
+  //         status: DONATION_STATUS.PAIED,
+  //       },
+  //     }
+  //   );
+  // }
 
   ctx.body = {};
   ctx.status = 200;
-}
+};
